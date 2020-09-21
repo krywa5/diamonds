0e7447f7b4a426a823b4830af750a6e7a7f17948
Krywa5 <blocker92@o2.pl>
2020-09-21 20:19:40 +0200

    init commit

---
+++ scripts/MainMenu.esm.js
@@ -0,0 +1,43 @@
+import { Common, HIDDEN_CLASS, VISIBLE_SCREEN } from './Common.esm.js';
+
+const SCALE_PROPERTY = '--scale-value';
+const START_SCREEN_SETTINGS_BUTTON_ID = 'js-settings-button';
+const START_SCREEN_GAME_BUTTON_ID = 'js-start-game';
+const START_SCREEN_ID = 'js-start-screen';
+
+
+class MainMenu extends Common {
+    constructor() {
+        super(START_SCREEN_ID);
+        this.bindToGameElements();
+        this.resizeGameWindow();
+        window.addEventListener('resize', this.resizeGameWindow);
+    }
+
+
+    bindToGameElements() {
+        const gameStartButton = this.bindToElement(START_SCREEN_GAME_BUTTON_ID);
+        const gameSettingsButton = this.bindToElement(START_SCREEN_SETTINGS_BUTTON_ID);
+
+        gameStartButton.addEventListener('click', this.showLevelScreen);
+        gameSettingsButton.addEventListener('click', this.showSettingsScreen);
+    }
+
+    showLevelScreen() {
+        console.log('wybór poziomu');
+    }
+
+    showSettingsScreen() {
+        console.log('ustawienia');
+    }
+
+    resizeGameWindow() {
+        const { innerWidth: width, innerHeight: height } = window;
+        const scale = Math.min(width / 640, height / 480);
+
+        document.documentElement.style.setProperty(SCALE_PROPERTY, scale);
+
+    }
+}
+
+export const mainMenu = new MainMenu();
